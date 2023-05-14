edfe014fc4e9f8f499720637e192104659103acd
Fim Vainqueur Ilunga <ilungafranck236@gmail.com>
2023-05-14 19:28:13 +0200

    integration complet du templates

--- app_restaurant/urls.py
+++ app_restaurant/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import home
+from .views import *
 
 urlpatterns = [
     
-    path('', home, name="home")
+    path('', home, name="home"),
+    path('menu/', menu, name="menu"),
+    path('about/', about, name="about"),
+    path('book/', book, name="book"),
 ]
--- app_restaurant/views.py
+++ app_restaurant/views.py
@@ -3,3 +3,13 @@ from django.shortcuts import render
 # Create your views here.
 def home(request, template_name='restaurant_templates/index.html'):
     return render(request,template_name)
+
+def menu(request, template_name='restaurant_templates/menu.html'):
+    return render(request,template_name)
+
+
+def about(request, template_name='restaurant_templates/about.html'):
+    return render(request,template_name)
+
+def book(request, template_name='restaurant_templates/book.html'):
+    return render(request,template_name)
